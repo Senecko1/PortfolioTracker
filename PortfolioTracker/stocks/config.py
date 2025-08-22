123272982b47a39e8a701108aa361f7ee3a6119b
Senecko1 <slamajakub@email.cz>
2025-08-22 12:56:54 +0200

    Added config.py

---
+++ PortfolioTracker/stocks/config.py
@@ -0,0 +1,30 @@
+# config.py
+
+CURRENCY_SYMBOLS = {
+    'USD': '$',   # US Dollar
+    'EUR': '€',   # Euro
+    'GBP': '£',   # British Pound
+    'CZK': 'Kč',  # Czech Koruna
+    'JPY': '¥',   # Japanese Yen
+    'CHF': 'CHF', # Swiss Franc
+    'CAD': 'C$',  # Canadian Dollar
+    'AUD': 'A$',  # Australian Dollar
+    'CNY': '¥',   # Chinese Yuan
+    'INR': '₹',   # Indian Rupee
+    'KRW': '₩',   # South Korean Won
+    'BRL': 'R$',  # Brazilian Real
+    'MXN': '$',   # Mexican Peso
+    'ZAR': 'R',   # South African Rand
+    'SEK': 'kr',  # Swedish Krona
+    'NOK': 'kr',  # Norwegian Krone
+    'TRY': '₺',   # Turkish Lira
+}
+
+TRANSACTION_TYPES = (
+    ('BUY', 'Buy'),
+    ('SELL', 'Sell'),
+)
+
+PRICE_REFRESH_DELTA_MINUTES = 15
+
+DATE_FORMAT_DISPLAY = 'd.m.Y'
